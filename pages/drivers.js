8420c1fb34ba7139deaa9b67d272b00ac31fcbef
louieheaton17 <louie@symplie.design>
2022-09-10 10:08:46 +0100

    Add drivers

--- pages/drivers.js
+++ pages/drivers.js
@@ -3,19 +3,18 @@ import Link from 'next/link'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+export default function Drivers() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Drivers page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Some drivers page!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-          <Link href="/drivers">Drivers</Link>
+          <Link href="/">Home</Link>
         </p>
       </main>
 
